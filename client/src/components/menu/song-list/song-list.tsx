d4091ae7262e5a29304bc3f15f2c294ae5b89c71
rzru <rzzzzru@gmail.com>
2026-03-20 20:20:22 +0100

    fix: small fixes for build & paddings

--- client/src/components/menu/song-list/song-list.tsx
+++ client/src/components/menu/song-list/song-list.tsx
@@ -29,23 +29,25 @@ export const SongList = ({ songsStore }: Props) => {
     <div className="flex min-h-0 w-full flex-1 justify-center">
       <div className="flex min-h-0 w-3/5 flex-col gap-4 p-4">
         <Filters />
-        <Progress songsStore={songsStore} />
-        <div ref={parentRef} className="flex-1 overflow-auto p-1" role="list">
-          <div
-            className="relative w-full"
-            style={{ height: virtualizer.getTotalSize() }}
-          >
-            {virtualizer.getVirtualItems().map((virtualItem) => (
-              <div
-                key={processed[virtualItem.index].title}
-                ref={virtualizer.measureElement}
-                data-index={virtualItem.index}
-                className="absolute left-0 w-full"
-                style={{ transform: `translateY(${virtualItem.start}px)` }}
-              >
-                <SongCard song={processed[virtualItem.index]} />
-              </div>
-            ))}
+        <div className="flex flex-col gap-2">
+          <Progress songsStore={songsStore} />
+          <div ref={parentRef} className="flex-1 overflow-auto p-1" role="list">
+            <div
+              className="relative w-full"
+              style={{ height: virtualizer.getTotalSize() }}
+            >
+              {virtualizer.getVirtualItems().map((virtualItem) => (
+                <div
+                  key={processed[virtualItem.index].title}
+                  ref={virtualizer.measureElement}
+                  data-index={virtualItem.index}
+                  className="absolute left-0 w-full"
+                  style={{ transform: `translateY(${virtualItem.start}px)` }}
+                >
+                  <SongCard song={processed[virtualItem.index]} />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
