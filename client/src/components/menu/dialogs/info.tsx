a7c908ae28e1a3e0b9c82aa8e7eec5c116c6328a
rzru <razzaru@yandex.ru>
2026-03-25 11:24:56 +0100

    fix: correct discord invite link

--- client/src/components/menu/dialogs/info.tsx
+++ client/src/components/menu/dialogs/info.tsx
@@ -68,7 +68,7 @@ export const InfoDialog = () => {
                 className={cn(
                   'focus-visible:ring-0 focus-visible:border-transparent',
                   focusedIndex === 0 &&
-                    'outline-2 outline-primary outline-offset-2',
+                  'outline-2 outline-primary outline-offset-2',
                 )}
               >
                 <GithubIcon className="size-4" />
@@ -76,7 +76,7 @@ export const InfoDialog = () => {
               <Button
                 variant="ghost"
                 size="icon-sm"
-                onClick={() => openUrl('https://discord.gg/2s2XGSQQ')}
+                onClick={() => openUrl('https://discord.gg/342GdJDk')}
                 className={cn(
                   'focus-visible:ring-0 focus-visible:border-transparent',
                   focusedIndex === 1 && 'ring-2 ring-primary',
